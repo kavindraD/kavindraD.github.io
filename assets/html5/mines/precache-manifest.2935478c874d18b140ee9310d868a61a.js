d7742f7aff63cef6cb33263124e288ab7a634fe9
Kavindra Dubey <kavindra.dubey@rediffmail.com>
2019-02-04 19:00:36 +0530

    Updated for IFRAME integartion with FrameWork

---
+++ assets/html5/mines/precache-manifest.2935478c874d18b140ee9310d868a61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fec5dc15eae328dbe3f8",
+    "url": "/assets/html5/mines/static/js/runtime~main.fec5dc15.js"
+  },
+  {
+    "revision": "14facca4b793942236e8",
+    "url": "/assets/html5/mines/static/js/main.14facca4.chunk.js"
+  },
+  {
+    "revision": "15c61f723f2717ce0517",
+    "url": "/assets/html5/mines/static/js/1.15c61f72.chunk.js"
+  },
+  {
+    "revision": "14facca4b793942236e8",
+    "url": "/assets/html5/mines/static/css/main.cee14e9e.chunk.css"
+  },
+  {
+    "revision": "15fd204083d572d0c68f5e2dbb19be38",
+    "url": "/assets/html5/mines/index.html"
+  }
+];
